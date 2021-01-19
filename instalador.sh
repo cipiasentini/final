36e4a00b565646c95dac0752efac873e5f962168
Carlos <cipiasentini@gmail.com>
2021-01-19 08:27:51 -0300

    updated up to 2020

--- instalador.sh
+++ instalador.sh
@@ -6,12 +6,12 @@ sudo apt-get install -y libzmq3-dev
 pip install pyinstaller
 pip install zerorpc
 sudo apt install -y curl
-curl -sL https://deb.nodesource.com/setup_9.x | sudo -E bash -
+curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
 sudo apt-get install -y nodejs
 sudo apt-get install -y build-essential
 git clone https://github.com/cipiasentini/final.git
 cd final
-npm install --runtime=electron --target=1.7.9
+npm install --runtime=electron --target=1.8.8
 pyinstaller apriori/server.py --distpath aprioriDistribucion
 rm -rf build/
 rm -rf server.spec
